39c1993acb6ef2575745fa638eb927bdbaf9620f
Sandra Dizdarevic <sdizdarevic@unomaha.edu>
2017-09-11 19:28:18 -0500

    HOpefully the last change.

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -26,7 +27,6 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
